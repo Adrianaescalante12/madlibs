c0ad50e430d455077f02ac2c75069ebfdf22aed9
Adrianaescalante12 <aescala097@gmail.com>
2021-08-18 03:57:22 +0000

    madlibs complete, further study started

--- madlibs.py
+++ madlibs.py
@@ -49,6 +49,20 @@ def show_madlib_form():
     else:
         return render_template("goodbye.html")
 
+@app.route('/madlib')
+def show_madlib():
+    name = request.args.get("name")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html",
+                            name = name,
+                            color = color,
+                            noun = noun,
+                            adjective = adjective)
+
+
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
